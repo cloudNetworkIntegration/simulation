d66299481372bef3c142721fbed288bceedbad8b
hardhpp <1983624597@qq.com>
2017-08-29 16:56:23 +0800

    页面三 适应性

--- new/js/main.js
+++ new/js/main.js
@@ -158,4 +158,56 @@ $.fn.fontScroll = function (options) {//WIFI探针滚动方法
         removeStyle();//移除所有列表的类
         setStyle();//给当前中间项以及前后2项设置类
     }
+
+    /*********************************storage模块******************************/
+    var canvas = $("canvas");
+    var ctx = canvas[0].getContext('2d');
+    var startTime = Date.now();
+    var time = 2000;
+    var clockwise = 1;
+    var cp1x, cp1y, cp2x, cp2y;
+
+    requestAnimationFrame(function waveDraw() {
+        var t = Math.min(1.0, (Date.now() - startTime) / time);
+
+        if(clockwise) {
+            cp1x = 121 + (74 * t);
+            cp1y = 28 + (72 * t);
+            cp2x = 123 - (69 * t);
+            cp2y = 179 - (79 * t);
+        } else {
+            cp1x = 195 - (74 * t);
+            cp1y = 100 - (72 * t);
+            cp2x = 54 + (69 * t);
+            cp2y = 100 + (79 * t);
+        }
+
+        ctx.clearRect(0, 0, 270, 200);
+        ctx.beginPath();
+        ctx.moveTo(0, 100);
+        // 绘制三次贝塞尔曲线
+        ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, 270, 100);
+        // 绘制矩形
+        ctx.rect(0, 100, 270,200);
+        ctx.fillStyle = '#bae3f9';
+        ctx.fill();
+        ctx.save();
+
+        if( t == 1 ) {
+            startTime = Date.now();
+            clockwise = !clockwise;
+        }
+
+        //水滴的运动效果
+        var small = $('.water-circle-small');
+        function moveLeft(elemt){
+            var initLeft = elemt.offsetLeft;
+            //      alert(initLeft);
+        }
+
+        moveLeft(small);
+
+        requestAnimationFrame(waveDraw);
+    });
+
 };
