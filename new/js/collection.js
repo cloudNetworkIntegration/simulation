c1dcb63b3f149677e06dd1eebf9d03a7990bedef
Bonnenu <283026381@qq.com>
2017-09-06 13:17:24 +0800

    -优化代码 -设置执行滚动方法的flag -由一次性获取数据改为每五秒获取前十秒的数据，故删除结束标志以及循环方法，将新增的数据添加至原数据后（问题：长时间停留页面将变得不流畅） -视频帧数改为计算得出，由当前时间相对零点的秒数乘以25得出

--- new/js/collection.js
+++ new/js/collection.js
@@ -1,6 +1,46 @@
 /********************采集端页面js，wifi探针的轮播************************/
+function formateTime(s) {
+    return s < 10 ? '0' + s : s;
+}
+
+function previousDate(now, time) {//dateStr格式为yyyy-mm-dd hh:mm:ss
+    var dt = new Date(now.replace(/-/, "/"));//将传入的日期格式的字符串转换为date对象 兼容ie
+    var ndt = new Date(dt.getTime() - time);//将转换之后的时间减去时间
+    var result = {
+        year: parseInt(ndt.getFullYear()),
+        month: parseInt(ndt.getMonth() + 1),
+        date: parseInt(ndt.getDate()),
+        h: parseInt(ndt.getHours()),
+        m: parseInt(ndt.getMinutes()),
+        s: parseInt(ndt.getSeconds())
+    };
+    return result;
+}
+
+function getNowFormatDate() {
+    var myDate = new Date();
+    var t = {
+        year: myDate.getFullYear(),
+        month: myDate.getMonth() + 1,
+        date: myDate.getDate(),
+        h: myDate.getHours(),
+        m: myDate.getMinutes() - 10,
+        s: myDate.getSeconds()
+    };
+    var now = t.year + '-' + formateTime(t.month) + '-' + formateTime(t.date) + ' ' + formateTime(t.h) + ':' + formateTime(t.m) + ':' + formateTime(t.s);
+    var result = previousDate(now, 10000);
+    now = now.replace(/[-: ]/g, '');
+    var pre = result.year + formateTime(result.month) + formateTime(result.date) + formateTime(result.h) + formateTime(result.m) + formateTime(result.s);
+    var time = {
+        now: now,
+        pre: pre
+    };
+    return time;
+}
+
 $(function () {
     var $elements = $("#wifi-data");
+    var flag = false;
     $.fn.fontChange = function () {
         $elements.find('._small').animate({
             fontSize: 16,
@@ -31,10 +71,6 @@ $(function () {
     };
 
     function moveUp() {
-        if ($elements.find('.stop').hasClass('_small')) {
-            $elements.css('top', 0 + 'px');
-            styleInit();
-        }
         $elements.animate({
             top: '-=' + lineHeight + 'px'
         });
@@ -48,35 +84,8 @@ $(function () {
         $elements.find('li:eq(3)').addClass(types[2]);
     }
 
-    $.ajax({//获取WIFI探针数据
-        url: 'data/wifiData.php',
-        dataType: 'json',
-        data: {
-            mac: "62:68:75:AA:11:05",
-            startTime: "20170728110000",
-            endTime: "20170728120000"
-        },
-        success: function (result) {
-            var data = result.WIFI;
-            var num = result.number;
-            var footData = data.slice(0, 3);//截取WIFI前3项data
-            var headData = data.slice(-2);//截取WIFI最后2项data
-            for (var i = headData.length - 1; i >= 0; i--) {//将headData倒序添加到data列表头部
-                data.unshift(headData[i]);
-            }
-            footData.forEach(function (t) {//将footData顺序添加到data列表底部
-                data.push(t);
-            });
-            for (i = 0; i < data.length; i++) {//将data每一项的内容添加到列表中
-                $("#wifi-data").append("<li>" + data[i].data + "</li>");
-            }
-            startScroll();
-            $('.wifi-content-title').find('p').eq(-1).html("U-ID DETECTTED：" + num);
-        }
-    });
-
     function startScroll() {
-        $elements.children().eq(-4).addClass('stop');//设置停止标记
+        flag = true;
         styleInit();
         lineHeight = $elements.children().eq(0).height();
         var time = setInterval(moveUp, 2000);
@@ -94,4 +103,51 @@ $(function () {
             time = setInterval(moveUp, 2000);
         };
     }
+
+    setInterval(function () {
+        var time = getNowFormatDate();
+        var pre = time.pre;
+        var now = time.now;
+        $.ajax({//获取WIFI探针数据
+            url: 'data/wifiData.php',
+            dataType: 'json',
+            data: {
+                mac: "62:68:75:AA:11:05",
+                startTime: pre,
+                endTime: now
+            },
+            success: function (result) {
+                var data = result.WIFI;
+                var num = result.number;
+                for (var i = 0; i < data.length; i++) {//将data每一项的内容添加到列表中
+                    $("#wifi-data").append("<li>" + data[i].data + "</li>");
+                }
+                $('.wifi-content-title').find('p').eq(-1).html("U-ID DETECTTED：" + num);
+                if (!flag) {
+                    startScroll();
+                }
+            },
+            error: function () {
+                var data = 'No data';
+                for (var i = 0; i < 5; i++) {//将data每一项的内容添加到列表中
+                    $("#wifi-data").append("<li>" + data + "</li>");
+                }
+                $('.wifi-content-title').find('p').eq(-1).html("U-ID DETECTTED：NULL");
+                styleInit();
+            }
+        });
+    }, 5000);
+    setInterval(function () {
+        //得到现在的时间
+        var today = new Date();
+        //得到今天零点的时间
+        var date = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+        //得到今天零点的毫秒
+        var zero = date.getTime();
+        //得到现在时间的毫秒
+        var now = today.getTime();
+        //得到零点到现在的秒数
+        var seconds = parseInt((now - zero) / 1000);
+        $('.monitor-content-title').find('p').eq(-1).html("FRAME PROCESSD：" + seconds * 25)
+    }, 1000);
 });
